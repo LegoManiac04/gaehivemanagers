eb77fffc902c07b765a59d9fbbd050aa788ca2d1
Leo <87787046+LegoManiac04@users.noreply.github.com>
2022-03-10 11:28:04 -0800

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,4 @@
 <template>
-<head>
-    
-    <title id="title">Gaehive Manager Portal</title>
-    <link rel="icon" href="./favicon.png">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-    
-</head>
-
 <div class="stripe"></div>
 
 <div class="hostqueue" id="hostqueue">
