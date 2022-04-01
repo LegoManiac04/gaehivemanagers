dcb28c3f05fc3a19a00e1de771fd5fdc2d53fd38
Leo <87787046+LegoManiac04@users.noreply.github.com>
2022-04-01 02:13:14 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
   <head>
     <title id="title">Gaehive Manager Portal</title>
-    <link rel="icon" href="./favicon.png">
+    <link rel="icon" href="/favicon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   </head>
 
