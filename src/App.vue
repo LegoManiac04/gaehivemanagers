df8702116163acbb8ddf45465f0ed27c7d564d3b
Leo <87787046+LegoManiac04@users.noreply.github.com>
2022-04-01 11:55:44 -0700

    Create App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,3 @@
-<head>
-  <title id="title">Gaehive Manager Portal</title>
-  <link rel="icon" href="./favicon.png">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-</head>
-
 <template>
   <router-view />
 </template>
