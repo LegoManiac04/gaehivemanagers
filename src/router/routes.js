15c7a24f8ae9d97e958ade055282c09e7ff4d682
Leo <87787046+LegoManiac04@users.noreply.github.com>
2022-04-25 11:45:38 -0700

    Router Fix

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,21 @@
-import Home from './views/Home.vue'
-import Faq from './views/Faq.vue'
-import NotFound from './views/404.vue'
+import Home from '../pages/Home.vue'
+import Faq from '../pages/Faq.vue'
+import Links from '../pages/Links.vue'
+import NotFound from '../pages/404.vue'
 
-
-/** @type {import('vue-router').RouterOptions['routes']} */
 export const routes = [
-  { path: '/', component: Home, meta: { title: 'Home' } },
+  { path: '/', 
+   component: Home,  
+  },
   {
     path: '/faq',
-    meta: { title: 'Faq' },
     component: Faq,
-    // example of route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import('./views/About.vue')
   },
-  { path: '/:path(.*)', component: NotFound },
+  {
+    path: '/links',
+    component: Links,
+  },
+  { path: '/:path(.*)',
+    component: NotFound 
+  },
 ]
