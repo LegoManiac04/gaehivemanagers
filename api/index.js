273782381af30dd504abc84136b88f85cf911295
Leo <87787046+LegoManiac04@users.noreply.github.com>
2022-05-28 01:54:04 -0700

    Update

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const app = require('express')();
 
+app.use(express.static(__dirname + '/src/styles'));
+
 app.get('/api', (req, res) => {
   res.send(`
-    <link rel="stylesheet" type="text/css" href="../style.css" />
+    <link rel="stylesheet" type="text/css" href="./style.css" />
     <h1>Test</h1>
     <p>Head to <a href="/login" target="_blank">/login</a> to get started.</p>
   `);
