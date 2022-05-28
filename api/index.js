947ba51528120ddf6ff33fba99be254d8cc2b3a6
Leo <87787046+LegoManiac04@users.noreply.github.com>
2022-05-28 01:38:33 -0700

    Tes

--- api/index.js
+++ api/index.js
@@ -1,3 +1,13 @@
-export default function handler(req, res) {
-  res.status(200).json({ hello: 'world' })
-}
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + './src/styles'));
+
+app.get('/', (req, res) => {
+  res.send(`
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <h1>Tes</h1>
+    <p>Head to <a href="/login" target="_blank">/login</a> to get started.</p>
+  `);
+});
