049f999c7188253835bef23c1cc24e136f21822d
Leo <87787046+LegoManiac04@users.noreply.github.com>
2022-05-28 12:26:26 -0700

    Fix

--- api/login/index.js
+++ api/login/index.js
@@ -3,6 +3,18 @@ const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const fetch = require('node-fetch');
 
+app.use(cookieParser());
+
+// Our default cookie options
+const cookieOptions = {
+    path: '/',                // Send the cookie to all routes
+    httpOnly: true,           // Make the cookie not accessible by document.cookie
+    sameSite: 'lax',          // Allow incoming links (but not requests) to include the cookie
+    secure: true,             // Make sure the cookie is secure
+    maxAge: 20 * 24 * 3600 * 1000  // The cookie expires in 20 days
+  };
+  
+
 // The /auth route
 app.get('/api/login', async (req, res) => {
     let data;
--- api/logout/index.js
+++ api/logout/index.js
@@ -3,6 +3,17 @@ const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const fetch = require('node-fetch');
 
+app.use(cookieParser());
+
+// Our default cookie options
+const cookieOptions = {
+    path: '/',                // Send the cookie to all routes
+    httpOnly: true,           // Make the cookie not accessible by document.cookie
+    sameSite: 'lax',          // Allow incoming links (but not requests) to include the cookie
+    secure: true,             // Make sure the cookie is secure
+    maxAge: 20 * 24 * 3600 * 1000  // The cookie expires in 20 days
+  };
+  
 
 // Clear the JWT cookie and redirect back to the index page
 app.get('/api/logout', (req, res) => {
