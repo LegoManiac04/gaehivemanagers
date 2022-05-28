22d97ca9290e7d0eb2859e517e2bd01d37b602bd
Leo <87787046+LegoManiac04@users.noreply.github.com>
2022-05-28 01:41:18 -0700

    fix?

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const app = require('express')();
 
-const app = express();
+app.get('/api', (req, res) => {
 
 app.use(express.static(__dirname + './src/styles'));
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send(`
     <link rel="stylesheet" type="text/css" href="style.css" />
     <h1>Tes</h1>
