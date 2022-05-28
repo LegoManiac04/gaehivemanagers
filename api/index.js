3481c791f27af277bc11f86f436bbc14e0d29b47
Leo <87787046+LegoManiac04@users.noreply.github.com>
2022-05-28 01:56:30 -0700

    Fix

--- api/index.js
+++ api/index.js
@@ -1,10 +1,7 @@
 const app = require('express')();
 
-app.use(express.static(__dirname + '/src/styles'));
-
 app.get('/api', (req, res) => {
   res.send(`
-    <link rel="stylesheet" type="text/css" href="./style.css" />
     <h1>Test</h1>
     <p>Head to <a href="/login" target="_blank">/login</a> to get started.</p>
   `);
