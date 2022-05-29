4d3a5fc17d1e608537c43afe437fcb732299348f
Leo <87787046+LegoManiac04@users.noreply.github.com>
2022-05-28 23:18:38 -0700

    Fix?

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const app = require('express')();
 app.get('/api', (req, res) => {
   res.send(`
     <style lang="scss">
-      @import "./styles.scss";
+      @import "../styles.scss";
     </style>
     <h1>Test</h1>
     <p>Head to <a href="/login" target="_blank">/login</a> to get started.</p>
